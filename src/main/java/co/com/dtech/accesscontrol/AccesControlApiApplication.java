ec863c51029d62e114147c5d9349f6526ed72bb4
Jonathan Barrera <andres.barar@gmail.com>
2022-05-15 23:15:29 -0500

    First commit

---
+++ src/main/java/co/com/dtech/accesscontrol/AccesControlApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.dtech.accesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/dtech/accesscontrol/AccesControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.dtech.accesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
